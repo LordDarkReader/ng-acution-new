c6046beecd11f8bd1cbfb7931d3b7cddb23dea5d
Cezary.Polosak <cezary.polosak@plus.pl>
2022-03-17 16:43:16 +0100

    add auth interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ProductListComponent } from './components/product-list/product-list.component';
-import { HttpClientModule } from '@angular/common/http';
-import { ProductService } from 'src/app/services/product-service';
-import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
-import { SearchComponent } from './components/search/search.component';
-import { ProductDetailsComponent } from './components/product-details/product-details.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { CartStatusComponent } from './components/cart-status/cart-status.component';
-import { CartDetailsComponent } from './components/cart-details/cart-details.component';
-import { CheckoutComponent } from './components/checkout/checkout.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { LoginComponent } from './components/login/login.component';
-import { LoginStatusComponent } from './components/login-status/login-status.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ProductListComponent} from './components/product-list/product-list.component';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {ProductService} from 'src/app/services/product-service';
+import {ProductCategoryMenuComponent} from './components/product-category-menu/product-category-menu.component';
+import {SearchComponent} from './components/search/search.component';
+import {ProductDetailsComponent} from './components/product-details/product-details.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {CartStatusComponent} from './components/cart-status/cart-status.component';
+import {CartDetailsComponent} from './components/cart-details/cart-details.component';
+import {CheckoutComponent} from './components/checkout/checkout.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {LoginComponent} from './components/login/login.component';
+import {LoginStatusComponent} from './components/login-status/login-status.component';
 import {OKTA_CONFIG, OktaAuthModule} from '@okta/okta-angular';
 import {Router} from '@angular/router';
 import myAppConfig from 'src/app/config/my-app-config';
-import { MembersPageComponent } from './components/members-page/members-page.component';
-import { OrderHistoryComponent } from './components/order-history/order-history.component';
+import {MembersPageComponent} from './components/members-page/members-page.component';
+import {OrderHistoryComponent} from './components/order-history/order-history.component';
+import {AuthInterceptorService} from 'src/app/services/auth-interceptor.service';
 
 const oktaConfig = Object.assign({
   onAuthRequired: (oktaAuth, injector) => {
@@ -52,7 +53,12 @@ const oktaConfig = Object.assign({
     ReactiveFormsModule,
     OktaAuthModule
   ],
-  providers: [ProductService, {provide: OKTA_CONFIG, useValue: oktaConfig}],
+  providers: [ProductService, {provide: OKTA_CONFIG, useValue: oktaConfig}, {
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthInterceptorService,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
