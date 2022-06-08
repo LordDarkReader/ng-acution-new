f5b42baa6cbb6317686042e32603c55b59f72fc4
Cezary.Polosak <cezary.polosak@plus.pl>
2022-06-08 11:29:07 +0200

    add property prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  devApiUrl: 'http://backend-ecommerce-service:8080/api',
+  stripePublishableKey: 'pk_test_51KfieNGDccDFASl56YskvRGceY1uKeSjAhawrqJhlWstQLSiRXbw6D09kSYQC89xKniJqF06NcKvOqblXvz8W6Ip00BdkCGH0A'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  devApiUrl: 'https://localhost:8443/api',
+  devApiUrl: 'http://backend-ecommerce-service:8080/api',
   stripePublishableKey: 'pk_test_51KfieNGDccDFASl56YskvRGceY1uKeSjAhawrqJhlWstQLSiRXbw6D09kSYQC89xKniJqF06NcKvOqblXvz8W6Ip00BdkCGH0A'
 };
 
